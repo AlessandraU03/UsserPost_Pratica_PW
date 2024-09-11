2a73ffbeff657c8f4b66e0b7247c69b89026c69d
AlessandraU03 <233303@ids.upchiapas.edu.mx>
2024-09-11 13:08:09 -0600

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';  // Para hacer peticiones HTTP
+import { FormsModule } from '@angular/forms';  // Asegúrate de importar FormsModule
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersModule } from './users/users.module';
-import { HttpClientModule } from '@angular/common/http';
+import { UsersDashboardComponent } from './users/users-dashboard/users-dashboard.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersDashboardComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    UsersModule
+    MatTableModule,
+    FormsModule,
+    MatButtonModule,   
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatSortModule
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
